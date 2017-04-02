3fe1760ba90ee5190de4a5f9baa138eb4fea3cdd
mpkerry <mkerry@g.harvard.edu>
2017-04-02 15:33:24 -0400

    section 9; incorporating spring security via plugin and moving some configuration there, annotating controllers, adding tags to views

--- grails-app/init/generateprice/BootStrap.groovy
+++ grails-app/init/generateprice/BootStrap.groovy
@@ -3,6 +3,7 @@ package generateprice
 class BootStrap {
 
     def init = { servletContext ->
+        println "******************* generateprice.BootStrap.groovy!!!! ***************"
     }
     def destroy = {
     }
--- src/main/groovy/generateprice/GeneratepriceGrailsPlugin.groovy
+++ src/main/groovy/generateprice/GeneratepriceGrailsPlugin.groovy
@@ -26,7 +26,7 @@ A very brief plugin to demonstrate use of custom plugin
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
@@ -38,7 +38,7 @@ A very brief plugin to demonstrate use of custom plugin
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
     Closure doWithSpring() { {->
             // TODO Implement runtime spring config (optional)
